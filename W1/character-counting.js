8808abe81d55422dc14e6bcb5fd66a694811c127
Esther Splett <estherina@gmail.com>
2019-03-06 11:19:49 -0500

    Add files via upload

---
+++ W1/character-counting.js
@@ -0,0 +1,16 @@
+function countLetters (word) {
+
+}
+
+
+countLetters("lighthouse in the house")
+
+
+
+
+// -countLetters return unique characters that exist
+// in a string that is passed into a function
+
+// -instances each letter was found in string
+// -returns object that represents stats for sentence,
+// all chars found in string, and their counts
