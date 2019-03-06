824a938997ee988ec0511432318d8ad139c95d9f
esther-splett <esthersplett@gmail.com>
2019-03-06 17:51:09 +0000

    solved character counting

--- W1/D3/character-counting.js
+++ W1/D3/character-counting.js
@@ -1,11 +1,31 @@
 function countLetters (word) {
+  var characters = {};
+  word = word.split(' ').join('')
 
+  for (var letter of word) {
+    if (!characters[letter]) {
+      characters[letter] = 0
+    }
+    characters[letter] ++
+  }
+  return characters
 }
 
+console.log(countLetters("lighthouse in the house"))
 
-countLetters("lighthouse in the house")
 
-
+// {
+//   l: 1,
+//   i: 2,
+//   g: 1,
+//   h: 4,
+//   t: 2,
+//   o: 2,
+//   u: 2,
+//   s: 2,
+//   e: 3,
+//   n: 1,
+// }
 
 
 // -countLetters return unique characters that exist
@@ -14,3 +34,5 @@ countLetters("lighthouse in the house")
 // -instances each letter was found in string
 // -returns object that represents stats for sentence,
 // all chars found in string, and their counts
+
+
