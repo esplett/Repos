e05b8534ed5efd489787431a0d125ace2f34ea18
esther-splett <esthersplett@gmail.com>
2019-03-11 20:27:07 +0000

    parsed the data into an object and am looping through the data to retrieve avatar URLS

--- W2/github-avatar-downloader/download_avatars.js
+++ W2/github-avatar-downloader/download_avatars.js
@@ -11,12 +11,21 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    //parse data into an object
+    var data = JSON.parse(body);
+      cb(err, data);
+        // console.log(data.avatar_url)
   });
 }
 
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  //instead of console logging results
+  //loop over array and print each avatar URL
+  for (i = 0; i < result.length; i++){
+    console.log(result[i].avatar_url)
+  }
 });
+
 
