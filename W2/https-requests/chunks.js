d6e71cfea35567cf16728d2c91aab45c3ed4807e
esther-splett <esthersplett@gmail.com>
2019-03-11 15:14:10 +0000

    wrote javascript that receives the data in chunks and concanates a new line

--- W2/https-requests/chunks.js
+++ W2/https-requests/chunks.js
@@ -1,3 +1,6 @@
+// while https is built-in to Node, it is a module, so it must be required
+var https = require('https');
+
 function getAndPrintHTMLChunks () {
 
   var requestOptions = {
@@ -6,8 +9,31 @@ function getAndPrintHTMLChunks () {
   };
 
   /* Add your code here */
+  // notice that https.get takes a callback with one
+  //parameter -
 
+     // response, which is a Stream that represents the HTTP response
+    https.get(requestOptions, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      console.log('Chunk Received:'  + '\n', data);
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+  });
 }
 
+getAndPrintHTMLChunks();
 
-This function should console.log each chunk of data as it is received, concatenated with a newline character ('\n') so you can visualize each chunk.
+
+
+//This function should console.log each chunk of data as it is received,
+//concatenated with a newline character ('\n') so you can visualize each chunk.
