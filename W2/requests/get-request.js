cf322c3c2074844590c46152e0ba86b2f98844f6
esther-splett <esthersplett@gmail.com>
2019-03-11 16:58:09 +0000

    made a get request to request an image and pipe it to read it and write it to a filename

---
+++ W2/requests/get-request.js
@@ -0,0 +1,25 @@
+// require `request` and the Node `fs` (filesystem) module
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+         //why here?
+         console.log('Download complete.')
+       })
+       .pipe(fs.createWriteStream('./future.jpg'));               // Note 4
+       console.log('Downloading image...')
+// Notes:
+// 1. `request.get` is equivalent to `request()`
+// 2. `request.on('error', callback)` handles any error
+// 3. `request.on('response, callback)` handles the response
+// 4. What is happening here?
+
+
+
+//which function could you chain to mark the end of
+//a writable stream?
