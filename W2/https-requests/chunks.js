59272c25fa8cdbdc6192dcbbf48e594addb9777d
esther-splett <esthersplett@gmail.com>
2019-03-11 15:03:14 +0000

    writing a function to make a get request that console logs data in chunks that are easy to read

---
+++ W2/https-requests/chunks.js
@@ -0,0 +1,13 @@
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+
+}
+
+
+This function should console.log each chunk of data as it is received, concatenated with a newline character ('\n') so you can visualize each chunk.
