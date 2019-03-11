880d6004b92aaaff3a1b6baf6440abcec641f1c5
esther-splett <esthersplett@gmail.com>
2019-03-11 18:53:34 +0000

    added gitignore file to ignore my user authentication

--- W2/github-avatar-downloader/download_avatars.js
+++ W2/github-avatar-downloader/download_avatars.js
@@ -3,11 +3,20 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
-}
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+    }
+  };
 
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
